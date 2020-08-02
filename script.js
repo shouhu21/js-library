09e910939e6956b40469ec8ecaea4482d3412bfd
shouhu <mbaazi@mailbox.org>
2020-08-02 19:26:12 +0200

    added variables and render function

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+// document variables
+const book-card   = document.querySelector('.library__book');
+const book-close  = document.querySelector('.library__close');
+const book-read   = document.querySelector('.library__read');
+const book-name   = document.querySelector('.library__name');
+const book-author = document.querySelector('.library__author');
+const book-pages  = document.querySelector('.library__pages');
+
 // array for book objects
 let myLibrary = [];
 
 // book constructor
 function book(title, author, pages, read) {
-  this.title = title,
+  this.title  = title,
   this.author = author,
-  this.pages = pages,
-  this.read = read,
-  this.info = function() {
-    return title + ' by ' + author + ' ' +  pages + ' pages, ' + read;
- };
+  this.pages  = pages,
+  this.read   = read
 }
 
-// function which renders the book object to html
+// adds a book to the library array
+function addBookToLibrary(title, author, pages, read) {
+  let Book = new book(title,author,pages,read);
+  myLibrary.push(Book);
+}
+
+// renders the book object to html
 function render() {
-  myLibrary.forEach(book(title, autor, pages, read) => {
-    book.info().querySelector('.library__book');
-  });
+
 }
 
 
-// function which adds a book to the library array
-const addBookToLibrary = (title, author, pages, read) => {
-  new book === title,author,pages,read;
-  myLibrary.push(book);
-}
+
+
+
+// TESTING: adds test book to library array
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'read');
