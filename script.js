5eed3b84c885db0662a77e0a33b9123b50b7b171
shouhu <mbaazi@mailbox.org>
2020-09-29 18:09:54 +0200

    added render function

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 // dom variables
-const Card = document.querySelector('.library__book');
-const Close = document.querySelector('.library__book-close');
-const Read = document.querySelector('.library__book-read');
-const Title = document.querySelector('.library__book-title');
-const Author = document.querySelector('.library__book-author');
-const Page = document.querySelector('.library__book-pages'); 
+const Card = document.querySelector(".library__book");
+const Close = document.querySelector(".library__book-close");
+const Read = document.querySelector(".library__book-read");
+const Title = document.querySelector(".library__book-name");
+const Author = document.querySelector(".library__book-author");
+const Page = document.querySelector(".library__book-pages");
 
 // array for book objects
 let myLibrary = [];
 
 // book constructor
 function book(title, author, pages, read) {
-  this.title  = title,
-  this.author = author,
-  this.pages  = pages,
-  this.read   = read
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
 }
 
 // adds a book to the library array
 function addBookToLibrary(title, author, pages, read) {
-  let Book = new book(title,author,pages,read);
+  let Book = new book(title, author, pages, read);
   myLibrary.push(Book);
 }
 
-
 // book button
-
-
-
 
 // book input form
 
-
-
 // TESTING: adds test books to library array
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295', 'read');
-addBookToLibrary('Shantaram', 'Gregory David Roberts', '936', 'not read');
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "read");
+//addBookToLibrary("Shantaram", "Gregory David Roberts", "936", "not read");
 
 // renders the book object properties to html
+function render() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    Title.innerHTML = book.title;
+    Author.innerHTML = "Author:<br>" + book.author;
+    Page.innerHTML = "Pages:<br>" + book.pages;
+  }
+}
 
+render()
