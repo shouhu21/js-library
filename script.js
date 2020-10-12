08b29df3e926cf3f8805b13bb6b16d9792d932cd
shouhu <mbaazi@mailbox.org>
2020-10-12 18:18:38 +0200

    added form input fields

--- script.js
+++ script.js
@@ -24,8 +24,23 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 // book button
+document
+  .querySelector(".button__book")
+  .addEventListener("click", activateForm());
+
+// activate form
+function activateForm() {
+  document.querySelector(".form-container").classList.toggle("active");
+}
 
 // book input form
+function bookInput() {
+  let bookTitle = document.getElementById("title").value;
+  let bookAuthor = document.getElementById("author").value;
+  let bookPages = document.getElementById("pages").value;
+  let bookRead = document.getElementById("read").value;
+  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+}
 
 // TESTING: adds test books to library array
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "read");
@@ -40,5 +55,3 @@ function render() {
     Page.innerHTML = "Pages:<br>" + book.pages;
   }
 }
-
-render()
