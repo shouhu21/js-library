676ddde0a4b5af024ebf1f14f82bab84eb68e51c
shouhu <mbaazi@mailbox.org>
2020-10-21 16:53:21 +0200

    improved render function & flex container

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// dom variables
-const Card = document.querySelector(".library__book");
-const Close = document.querySelector(".library__book-close");
-const Read = document.querySelector(".library__book-read");
-const Title = document.querySelector(".library__book-name");
-const Author = document.querySelector(".library__book-author");
-const Page = document.querySelector(".library__book-pages");
-
 // array for book objects
 let myLibrary = [];
+
+// // dom variables
+// const Card = document.createElement("div").classList.add("library__book");
+// const Close = document
+//   .createElement("div")
+//   .classList.add("library__book-close");
+// const Read = document.createElement("div").classList.add("library__book-read");
+// const Title = document.createElement("div").classList.add("library__book-name");
+// const Author = document
+//   .createElement("div")
+//   .classList.add("library__book-author");
+// const Page = document.createElement("div").classList.add("library__book-pages");
 
 // book constructor
 function book(title, author, pages, read) {
@@ -59,7 +63,9 @@ document.querySelector(".button__addbook").addEventListener("click", showForm);
 // submit book button
 document.querySelector(".button__submit").addEventListener("click", () => {
   getInput();
+  createCard();
   addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+  myLibrary.forEach(render);
   resetForm();
   hideForm();
 });
@@ -70,15 +76,46 @@ function resetForm() {
 }
 
 // TESTING: adds test books to library array
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "read");
+//addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "read");
 //addBookToLibrary("Shantaram", "Gregory David Roberts", "936", "not read");
 
+// creates book "card"
+function createCard() {
+  let libraryContainer = document.querySelector(".library");
+  let bookContainer = document.createElement("div");
+  let closeHTML = document.createElement("div");
+  let readHTML = document.createElement("div");
+  let nameHTML = document.createElement("div");
+  let authorHTML = document.createElement("div");
+  let pagesHTML = document.createElement("div");
+
+  bookContainer.classList.add("library__book");
+  closeHTML.classList.add("library__book-close");
+  readHTML.classList.add("library__book-read");
+  nameHTML.classList.add("library__book-name");
+  authorHTML.classList.add("library__book-author");
+  pagesHTML.classList.add("library__book-pages");
+
+  closeHTML.innerHTML = "<i class='fas fa-times'></i>";
+  readHTML.innerHTML = "<i class='fas fa-eye'></i>";
+  nameHTML.innerHTML = "";
+  authorHTML.innerHTML = "Author:";
+  pagesHTML.innerHTML = "Pages:";
+
+  libraryContainer.appendChild(bookContainer);
+  bookContainer.appendChild(closeHTML);
+  bookContainer.appendChild(readHTML);
+  bookContainer.appendChild(nameHTML);
+  bookContainer.appendChild(authorHTML);
+  bookContainer.appendChild(pagesHTML);
+}
+
 // renders the book object properties to html
-function render() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    Title.innerHTML = book.title;
-    Author.innerHTML = "Author:<br>" + book.author;
-    Page.innerHTML = "Pages:<br>" + book.pages;
-  }
+function render(book) {
+  let renderName = document.querySelector(".library__book-name");
+  let renderAuthor = document.querySelector(".library__book-author");
+  let renderPage = document.querySelector(".library__book-pages");
+  renderName.innerHTML = book.title;
+  renderAuthor.innerHTML = "Author:<br>" + book.author;
+  renderPage.innerHTML = "Pages:<br>" + book.pages;
 }
